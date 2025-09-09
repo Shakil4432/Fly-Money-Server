9b30bee0364768f371cb353bca5c7fb6c87c3364
Shakil4432 <shakil4432@gmail.com>
2025-09-09 18:21:23 +0600

    deploy

--- src/components/DeliveryBanner.tsx
+++ src/components/DeliveryBanner.tsx
@@ -3,18 +3,20 @@ import { Button } from "./ui/button";
 
 const DeliveryBanner = () => {
   return (
-    <div
-      className="container relative mx-auto rounded-sm my-10 h-[80vh]"
-      style={{
-        backgroundImage: "url('/delivery.png')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="flex items-center absolute top-[85%] justify-end  w-full">
-        <Button className="bg-white text-[#7c3f00] hover:bg-white  text-3xl font-bold mr-52">
-          Order Now
-        </Button>
+    <div className="px-2 lg:px-0">
+      <div
+        className="container relative mx-auto rounded-sm my-10 h-[25vh] lg:h-[80vh]"
+        style={{
+          backgroundImage: "url('/delivery.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="flex items-center absolute top-[75%]  lg:top-[85%] justify-end  w-full">
+          <Button className="bg-white text-[#7c3f00] hover:bg-white  lg:text-3xl font-bold mr-7 lg:mr-52">
+            Order Now
+          </Button>
+        </div>
       </div>
     </div>
   );
